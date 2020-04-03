3b5bf1fcb123ef3019d7e440344a9dbfd7f58e39
liongkj <fantasticbabii@gmail.com>
2020-04-04 00:35:12 +0800

    update ads

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ async function handleRequest(request) {
   adsList.push(
     new AdModel({
       imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/f/fd/Portdickson_014.jpg',
+        'https://res.cloudinary.com/jomn9-com/image/upload/v1578318038/ads/ad2_j6fpt4.jpg',
       linkTo: 'www.google.com',
       title: 'Port Dickson',
     }),
     new AdModel({
       imageUrl:
-        'https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.0-9/s960x960/84482301_161716748612400_9097443359512854528_o.jpg?_nc_cat=108&_nc_ohc=y9YL8Fr_pCsAX-M1NQG&_nc_ht=scontent.fmkz1-1.fna&oh=fe6830571dcfd52aabd34436a52c5265&oe=5F03A93C',
+        'https://res.cloudinary.com/jomn9-com/image/upload/v1578318037/ads/ad1_eowzgj.jpg',
       linkTo: 'https://www.facebook.com/SerembanTwo/posts/3723116394373102',
       title: 'Pasar Besar Gotong Royong',
     }),
